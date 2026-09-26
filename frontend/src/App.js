822716f59ebf0316960bbb830454a160055cfea8
Marcelo Moreira <mmds12@hotmail.com>
2026-09-26 17:13:14 +0000

    FINAL - suburbana baia + orla atlantica geografica

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,36 +7,47 @@ function App() {
     { nome: 'Av. Paralela', coords: [[-12.912, -38.345], [-12.932, -38.37], [-12.947, -38.398], [-12.962, -38.42], [-12.975, -38.445], [-12.985, -38.465]], status: 'congestionado', co2: '4.2 kg' },
     { nome: 'Av. ACM', coords: [[-12.96, -38.48], [-12.97, -38.485], [-12.984, -38.49], [-12.995, -38.47]], status: 'moderado', co2: '2.1 kg' },
     { nome: 'Bonocô', coords: [[-12.956, -38.481], [-12.968, -38.487], [-12.978, -38.491]], status: 'moderado', co2: '2.5 kg' },
-    // SUBURBANA - AGORA CURVADA BEIRANDO A BAÍA
-    { nome: 'Suburbana', coords: [
-      [-12.972, -38.513], // Comércio
-      [-12.950, -38.507], // Calçada / Mares
-      [-12.930, -38.500], // Roma / Boa Vista Lobato
-      [-12.910, -38.492], // Lobato / Plataforma
-      [-12.890, -38.483], // Plataforma / Itacaranha
-      [-12.870, -38.473], // Periperi
-      [-12.850, -38.462], // Paripe / Praia Grande
-      [-12.830, -38.450]  // Base Naval
-    ], status: 'livre', co2: '1.2 kg' },
-    // ORLA - 200M MAIS PRA DENTRO AINDA
-    { 
-      nome: 'Orla - Barra / Itapuã', 
+
+    // SUBURBANA - agora seguindo a orla da Baía de Todos os Santos (lado oeste da península)
+    {
+      nome: 'Suburbana',
       coords: [
-        [-12.998, -38.520], // Farol Barra - DENTRO
-        [-12.993, -38.515], // Cristo / Ondina
-        [-12.997, -38.502], // Rio Vermelho
-        [-12.996, -38.488], // Buracão
-        [-12.991, -38.472], // Amaralina
-        [-12.985, -38.458], // Pituba
-        [-12.978, -38.443], // Costa Azul / Armação
-        [-12.968, -38.428], // Boca do Rio / Imbuí
-        [-12.956, -38.412], // Pituaçu / Jaguaribe
-        [-12.944, -38.395], // Patamares / Piatã
-        [-12.930, -38.375]  // Itapuã
-      ], 
-      status: 'livre', 
-      co2: '0.9 kg' 
+        [-12.9718, -38.5117], // Comércio
+        [-12.9600, -38.5100], // Calçada
+        [-12.9470, -38.5075], // Roma
+        [-12.9330, -38.5000], // Boa Vista de Lobato
+        [-12.9150, -38.4930], // Lobato
+        [-12.8980, -38.4855], // Plataforma
+        [-12.8850, -38.4780], // Itacaranha
+        [-12.8680, -38.4670], // Periperi
+        [-12.8500, -38.4560], // Paripe
+        [-12.8320, -38.4430], // Praia Grande
+        [-12.8150, -38.4300], // Base Naval
+      ],
+      status: 'livre',
+      co2: '1.2 kg',
     },
+
+    // ORLA - agora seguindo a orla atlântica real, da Barra até Itapuã
+    {
+      nome: 'Orla - Barra / Itapuã',
+      coords: [
+        [-13.0103, -38.5326], // Farol da Barra
+        [-13.0060, -38.5230], // Porto da Barra
+        [-13.0028, -38.5133], // Ondina
+        [-12.9958, -38.4959], // Rio Vermelho
+        [-12.9890, -38.4802], // Amaralina
+        [-12.9836, -38.4630], // Pituba (orla)
+        [-12.9727, -38.4459], // Boca do Rio
+        [-12.9680, -38.4330], // Armação / Costa Azul
+        [-12.9560, -38.4050], // Jaguaribe / Piatã (trecho)
+        [-12.9450, -38.3780], // Piatã
+        [-12.9330, -38.3480], // Itapuã
+      ],
+      status: 'livre',
+      co2: '0.9 kg',
+    },
+
     { nome: 'Centro', coords: [[-12.971, -38.512], [-12.975, -38.514], [-12.978, -38.511]], status: 'congestionado', co2: '3.8 kg' },
     { nome: 'Garibaldi', coords: [[-13.002, -38.514], [-12.996, -38.51], [-12.988, -38.503]], status: 'moderado', co2: '2.0 kg' },
     { nome: 'Centenário / Barra', coords: [[-12.99, -38.505], [-12.998, -38.515], [-13.005, -38.525]], status: 'livre', co2: '1.0 kg' },
