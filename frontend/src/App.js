b37f6a4c2af20b791bbf667d29e2d6019f82fbf4
Marcelo Moreira <mmds12@hotmail.com>
2026-09-26 15:18:20 +0000

    orla dentro da cidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import 'leaflet/dist/leaflet.css';
 
 function App() {
   const rotas = [
-    { nome: 'Av. Paralela', coords: [[-12.908, -38.324], [-12.928, -38.35], [-12.944, -38.378], [-12.958, -38.41], [-12.972, -38.44], [-12.98, -38.463]], status: 'congestionado', co2: '4.2 kg' },
+    // PARALELA - corrigida pra dentro
+    { nome: 'Av. Paralela', coords: [[-12.912, -38.345], [-12.932, -38.37], [-12.947, -38.398], [-12.962, -38.42], [-12.975, -38.445], [-12.985, -38.465]], status: 'congestionado', co2: '4.2 kg' },
     { nome: 'Av. ACM', coords: [[-12.96, -38.48], [-12.97, -38.485], [-12.984, -38.49], [-12.995, -38.47]], status: 'moderado', co2: '2.1 kg' },
     { nome: 'Bonocô', coords: [[-12.956, -38.481], [-12.968, -38.487], [-12.978, -38.491]], status: 'moderado', co2: '2.5 kg' },
-    { nome: 'Suburbana', coords: [[-12.86, -38.485], [-12.89, -38.49], [-12.91, -38.498], [-12.924, -38.508], [-12.94, -38.51]], status: 'livre', co2: '1.2 kg' },
-    { nome: 'Orla Salvador', coords: [[-12.953, -38.36], [-12.969, -38.377], [-12.983, -38.394], [-12.994, -38.418], [-13.004, -38.437], [-13.011, -38.456], [-13.016, -38.477], [-13.012, -38.498], [-13.009, -38.515], [-13.003, -38.531]], status: 'livre', co2: '0.9 kg' },
+    // SUBURBANA - AGORA DENTRO DA TERRA, NÃO NO MAR DA BAÍA
+    { nome: 'Suburbana', coords: [[-12.87, -38.466], [-12.90, -38.475], [-12.92, -38.487], [-12.94, -38.494], [-12.97, -38.505]], status: 'livre', co2: '1.2 kg' },
+    // ORLA - AGORA 100% DENTRO DA CIDADE, COLADA NA AVENIDA OTAVIO MANGABEIRA
+    { nome: 'Orla Salvador', coords: [[-12.948, -38.352], [-12.961, -38.368], [-12.974, -38.385], [-12.986, -38.408], [-12.996, -38.435], [-13.005, -38.455], [-13.010, -38.48], [-13.008, -38.505], [-13.001, -38.522], [-13.007, -38.533]], status: 'livre', co2: '0.9 kg' },
     { nome: 'Centro', coords: [[-12.971, -38.512], [-12.975, -38.514], [-12.978, -38.511]], status: 'congestionado', co2: '3.8 kg' },
     { nome: 'Garibaldi', coords: [[-13.002, -38.514], [-12.996, -38.51], [-12.988, -38.503]], status: 'moderado', co2: '2.0 kg' },
     { nome: 'Centenário / Barra', coords: [[-12.99, -38.505], [-12.998, -38.515], [-13.005, -38.525]], status: 'livre', co2: '1.0 kg' },
